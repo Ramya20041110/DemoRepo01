7a9e13cf7393339f80257bb9c346e5ecfbb8f9cc
Ramya V <ramyavenkatesan65@gmail.com>
2026-08-02 20:28:42 +0530

    Update test.groovy

--- test.groovy
+++ test.groovy
@@ -1,21 +1,65 @@
 pipeline {
-    agent any
+    agent none
+    parameters {
+        string(name:'employee', defaultValue:'Ramya')
+        choice(name:'ops', choices:['Dev','QA','tester'])
+        booleanParam(name:'deploy', defaultValue:true)
+    }
     stages {
-        stage("Build"){
-            steps{
-                echo "Build Successfully"
-            }
+        
+        stage("Stage 1"){
+            agent{
+                    label 'ubun'
+                }
+            environment{
+            App_name="fruit"
+            Version01 ="1.0"
         }
-         stage("Test"){
             steps{
-                echo "Test Successfully"
-            }
+                echo "Application Name : ${env.App_name}"
+                echo "Version : ${env.Version01}"
+                }
+                
         }
-         stage("deploy"){
+        stage ("Stage 2"){
+            agent{
+                    label 'ubun'
+                }
+
             steps{
-                echo "deploy Successfully"
+                catchError(message: 'error occur still runs for processing other stage',buildResult: 'SUCCESS',stageResult: 'FAILURE') {
+                    sh "jjj"  
+                    
+                }
+                 
             }
+                
         }
+      
+        stage ("Stage 3"){
+            agent{
+                    label 'built'
+                }
+            steps{
+                 sh "uptime"
+                 echo "employee of the parameter ${params.employee}"
+                 echo "ops of the parameter ${params.ops}"
+            }
+                
+        }
+        stage ("Stage 4"){
+            agent{
+                    label 'ubun'
+                }
+            steps{
+                 sh "date"
+                 echo "ops of the parameter ${params.deploy}"
+            }
+                
+        }
+       
+
+        
     }
     post{
         always{
@@ -28,4 +72,5 @@ pipeline {
             echo "Failed test"
         }
     }
+
 }
