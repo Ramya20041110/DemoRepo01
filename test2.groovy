14fc6408bd4d6027cf79ae32e3cce99fe1b60172
Ramya V <ramyavenkatesan65@gmail.com>
2026-08-02 20:52:19 +0530

    Update and rename xyz to test2.groovy

---
+++ test2.groovy
@@ -0,0 +1,30 @@
+pipeline {
+
+    agent {
+        label 'ubun'
+    }
+
+    stages {
+
+        stage('Checkout') {
+            steps {
+                git branch: 'main',
+                    url: 'https://github.com/Ramya20041110/DemoRepo01'
+            }
+        }
+
+        stage('Build') {
+            steps {
+                echo 'Building application'
+            }
+        }
+
+        stage('Deploy') {
+            steps {
+                sh '''
+                sudo cp index.html /var/www/html/
+                '''
+            }
+        }
+    }
+}
