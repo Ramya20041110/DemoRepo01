6104b38af044f1d957282e02263a77d9407ddd18
Ramya V <ramyavenkatesan65@gmail.com>
2026-08-10 20:39:40 +0530

    Update Health-Monitor.sh

--- Health-Monitor.sh
+++ Health-Monitor.sh
@@ -1,21 +1,25 @@
-#! /bin/bash
+#!/bin/bash
 
 echo " ----------------------------------------------------"
-echo " Server Health Montioring Using Shell Scripting "
+echo " Server Health Monitoring Using Shell Scripting "
 echo " ----------------------------------------------------"
- 
-HOST_NAME = $(hostname)
-DATE = $(date)
-UPTIME = $(uptime)
-CPU_USAGE = $(top -bn1 | grep "Cpu(s)")
-MEMORY_USAGE = $(free -h)
-DISK = $(df)
-echo " HOST NAME : $HOST_NAME "
-echo " CURRENT DATE : $DATE "
-echo " UPTIME : $UPTIME "
-echo " CPU USUAGE : $CPU_USAGE "
-echo " MEMORY USUAGE : $MEMORY_USAGE "
-echo " DISK MONITORING : $DISK "
+
+HOST_NAME=$(hostname)
+DATE=$(date)
+UPTIME=$(uptime)
+CPU_USAGE=$(top -bn1 | awk '/Cpu\(s\)/ {print 100 - $8}')
+MEMORY_USAGE=$(free -h)
+DISK=$(df -h)
+
+echo " HOST NAME      : $HOST_NAME "
+echo " CURRENT DATE   : $DATE "
+echo " UPTIME         : $UPTIME "
+echo " CPU USAGE      : $CPU_USAGE% "
+echo " MEMORY USAGE   :"
+echo "$MEMORY_USAGE"
+echo " DISK MONITORING:"
+echo "$DISK"
+
 if [ "$CPU_USAGE" -gt 70 ]; then
     echo "WARNING: CPU usage is above 70%"
 else
