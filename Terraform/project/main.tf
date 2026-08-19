0309beba24f1fb22eed3330603426085ee1107b6
Ramya V <ramyavenkatesan65@gmail.com>
2026-08-19 20:02:34 +0530

    Update and rename terraform-project.tf to Terraform/project/main.tf

--- Terraform/project/main.tf
+++ Terraform/project/main.tf
@@ -2,32 +2,6 @@ provider "aws" {
     region = "ap-south-1"
 }
 
-variable "cidr-range" {
-    default = "10.0.0.0/16"
-}
-variable "cidr-sub" {
-    default = "10.0.0.0/24"
-}
-variable "cidr-subpvt" {
-    default = "10.0.1.0/24"
-}
-variable "ec2-ami" {
-    description = "Ami for Ec2 instance"
-    type = string
-    default = "ami-0199ac7c9fbf9ed83"
-}
-
-variable "inst-type"{
-    description = "Instance type for ec2"
-    type = string
-    default = "t3.micro"
-}
-
-variable "inst-name"{
-    description = "Name of the instance"
-    type = string
-    default = "Controller"
-}
 resource "aws_key_pair" "keypair1" {
     key_name = "terraform-key"
     public_key = file("~/.ssh/id_rsa.pub")
